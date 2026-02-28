d4ce947ecfe9175cb1a9ccc2d90de5c9e74cc721
mahbuba51 <mahbubasayed51@gmail.com>
2026-02-28 19:13:34 +0600

    Cart deleted from backend

--- backend/src/main/java/com/foodibd/backend/repository/CartRepository.java
+++
@@ -1,9 +0,0 @@
-package com.foodibd.backend.repository;
-
-import com.foodibd.backend.entity.Cart;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface CartRepository extends JpaRepository<Cart, Integer> {
-    Optional<Cart> findByCustomerCustomerID(Integer customerId);
-}
--- backend/src/main/java/com/foodibd/backend/repository/RiderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.foodibd.backend.repository;
-
-import com.foodibd.backend.entity.Rider;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RiderRepository extends JpaRepository<Rider, Integer> {
-}
